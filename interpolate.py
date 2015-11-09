38dde13130068897426a3fb747ad9d55938d1920
m-ciesielski <mateusz.jan.ciesielski@gmail.com>
2015-11-09 13:35:53 +0100

    Refactoring, add option to add node to polynomial

--- interpolate.py
+++ interpolate.py
@@ -10,17 +10,18 @@ class Node(object):
     left_parent = None
     right_parent = None
 
-    def __init__(self, left_parent=None, right_parent=None, x=None, value=None):
+    def __init__(self, left_parent=None, right_parent=None, x=None, divided_difference=None):
         self.left_parent = left_parent
         self.right_parent = right_parent
         self.x = x
-        self.divided_difference = value
+        self.divided_difference = divided_difference
 
     def __str__(self):
         return "Value: {0},".format(self.divided_difference)
 
     def calculate_value(self):
-        self.divided_difference = ((self.right_parent.value - self.left_parent.value) / (self.get_right_x() - self.get_left_x()))
+        self.divided_difference = ((self.right_parent.divided_difference - self.left_parent.divided_difference) / (
+        self.get_right_x() - self.get_left_x()))
 
     def get_left_x(self):
         if self.x is not None:
@@ -57,10 +58,9 @@ def calculate_initial_nodes(x_start, x_end, nodes_y):
 
 
 def calculate_divided_differences_row(nodes_to_compute):
-
     divided_differences = []
 
-    for i in range(0, len(nodes_to_compute)-1):
+    for i in range(0, len(nodes_to_compute) - 1):
         if len(nodes_to_compute) == 1:
             return None
         child = Node.create_child_node(nodes_to_compute[i], nodes_to_compute[i + 1])
@@ -78,7 +78,7 @@ def calculate_divided_differences(nodes):
     nodes_to_compute = []
     divided_differences = []
     for node in nodes:
-        nodes_to_compute.append(Node(x=node[0], value=node[1]))
+        nodes_to_compute.append(Node(x=node[0], divided_difference=node[1]))
 
     divided_differences.append(tuple(nodes_to_compute))
 
@@ -93,8 +93,7 @@ def calculate_divided_differences(nodes):
 def calculate_newton_interpolation(divided_differences):
     polynomial = []
     for i, divided_differences_row in enumerate(divided_differences):
-        polynomial_part = '({0})'.format(divided_differences_row[0].value)
-        print(polynomial_part)
+        polynomial_part = '({0})'.format(divided_differences_row[0].divided_difference)
         for j in range(0, i):
             polynomial_part += '*(x-{0})'.format(divided_differences[0][j].x)
 
@@ -110,23 +109,51 @@ def calculate_newton_interpolation(divided_differences):
     return simplified_polynomial
 
 
-def draw_interpolation_plot(interpolation_polynomial):
+def draw_interpolation_plot(interpolation_polynomial=None, initial_nodes=None, start_x=0, end_x=15, freq=200):
     plt.figure(figsize=(8, 6), dpi=80)
-    x = linspace(1, 5)
+    x = linspace(start_x, end_x, freq)
     # eval should be changed to something more secure (like numexpr evaluate())...
     y = eval(str(interpolation_polynomial))
-    print(y)
-    plt.plot(x, y)
+    if initial_nodes:
+        initial_x = []
+        initial_y = []
+        for node in initial_nodes:
+            initial_x.append(node[0])
+            initial_y.append(node[1])
+
+        plt.plot(x, y, initial_x, initial_y, 'ro')
+    else:
+        plt.plot(x, y)
+
     plt.grid(True)
     plt.show()
 
 
+def calculate_and_draw_newton_interpolation(args=None, nodes=None):
+    if nodes:
+        initial_nodes = calculate_initial_nodes(args.start, args.end, nodes)
+    else:
+        initial_nodes = calculate_initial_nodes(args.start, args.end, args.nodes)
+
+    divided_differences = calculate_divided_differences(initial_nodes)
+    interpolation_polynomial = calculate_newton_interpolation(divided_differences)
+    draw_interpolation_plot(start_x=args.start, end_x=args.end, interpolation_polynomial=interpolation_polynomial,
+                            initial_nodes=initial_nodes)
+
+
+def add_new_node_to_interpolation(polynomial, node):
+    """
+    TODO: implement addition of node to already calculated polynomial.
+    """
+    pass
+
+
 def parseargs():
     parser = argparse.ArgumentParser(description='Newton\'s Interpolation .')
-    parser.add_argument('--start', help='Beginning of X values range.')
-    parser.add_argument('--end', help='End of X values range.')
-    parser.add_argument('-n', help='Count of nodes.')
-    parser.add_argument('--nodes', nargs='+', help='Nodes Y values.')
+    parser.add_argument('--start', required=True, help='Beginning of X values range.')
+    parser.add_argument('--end', required=True, help='End of X values range.')
+    parser.add_argument('-n', required=False, help='Count of nodes.')
+    parser.add_argument('--nodes', required=True, nargs='+', help='Nodes Y values.')
     return parser.parse_args()
 
 
@@ -134,8 +161,12 @@ if __name__ == '__main__':
     args = parseargs()
     args.start = int(args.start)
     args.end = int(args.end)
-    # draw_initial_plot(calculate_nodes(args.start, args.end, args.nodes))
-    divided_differences = calculate_divided_differences(calculate_initial_nodes(args.start, args.end, args.nodes))
-    print(divided_differences)
-    interpolation_polynomial = calculate_newton_interpolation(divided_differences)
-    draw_interpolation_plot(interpolation_polynomial)
+
+    calculate_and_draw_newton_interpolation(args=args)
+
+    new_node_y = input("Pass new node Y value:")
+    new_nodes = args.nodes
+    new_nodes.append(new_node_y)
+
+    args.end += 1
+    calculate_and_draw_newton_interpolation(args=args, nodes=new_nodes)
